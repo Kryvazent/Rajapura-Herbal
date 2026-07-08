82e5c01ffac6d774ae26c0ddaf4200d0d14eaabe
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2026-07-08 20:53:44 +0530

    upload img

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,9 @@ app.use(cors({
 
         return callback(new Error("Not allowed by CORS"));
     },
-    credentials: true
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
